466881d4f56eb9cd85ed6f27e0cd7f4a8209fb17
Łukasz Hryniuk <code@hryniuk.pl>
2018-09-26 14:53:39 +0200

    Rename test case

--- tests/test_data_triple.py
+++ tests/test_data_triple.py
@@ -1,7 +1,7 @@
 import asm
 
 
-def test_instruction_is_converted_properly():
+def test_rs_instruction_is_converted_properly():
     svc_instruction = asm.Instruction(0x2e, ['1 0', '1'])
     converted = asm.to_data_triples(svc_instruction)
     expected = asm.DataTriple(4, 0, [0x2e, (0x01 << 4) | 0x01, 0x00, 0x00])
