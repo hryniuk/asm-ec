12802008e811e45659693d80054acf7af4b28d69
Łukasz Hryniuk <code@hryniuk.pl>
2020-11-19 18:27:42 +0100

    Fix formating

--- src/fconv.py
+++ src/fconv.py
@@ -9,21 +9,25 @@ import struct
 
 STRUCT_FMT = ">f"
 
+
 def float32_to_bytes(v):
     # Note: explicitly converting to float as it do not change behaviour, i.e.
     # float32_to_bytes(float(<int>)) == float32_to_bytes(<int>)
     # ...but might help in debugging.
     return list(struct.pack(STRUCT_FMT, float(v)))
 
+
 def bytes_to_float32(b):
     return struct.unpack(STRUCT_FMT, bytes(b))
+
 
 def float20_to_bytes(v):
     # Note: see comment in float32_to_bytes.
     r = float32_to_bytes(v)
-    r[3] = r[3] & 0xf0
+    r[3] = r[3] & 0xF0
     return r
 
+
 def bytes_to_float20(b):
-    assert (b[3] & 0x0f) == 0
+    assert (b[3] & 0x0F) == 0
     return struct.unpack(STRUCT_FMT, bytes(b))
